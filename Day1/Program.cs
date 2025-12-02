0bd8e0d7d9608d3db0aea98ea5e067f610e6745e
j.randle-argyle <j.randle-argyle@outlook.com>
2025-12-02 19:36:54 +0000

    Refactor

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,78 +1,89 @@
 ﻿
-var counter = 0;
-var startingPoint = 50;
+
 var operations = File.ReadAllLines("input.txt");
 
-foreach (var operation in operations)
+Part1(operations);
+
+Part2(operations);
+
+static void Part2(string[] operations)
 {
-    var t = operation[0];
-    var value = int.Parse(operation.Trim(t));
-
-    if (t.Equals('L'))
-    {
-        // Minus value from startingPoint
-        startingPoint -= value;
-    }
-    else if (t.Equals('R'))
-    {
-        // Add value to startingPoint
-        startingPoint += value;
-    }
-
-    if (startingPoint.Equals(0))
-    {
-        counter++;
-        continue;
-    }
-
-    if (startingPoint < 0)
-    {
-        startingPoint = (100 + startingPoint);
-    }
-
-    if ((startingPoint % 100) == 0)
-        counter++;
-}
-
-Console.WriteLine($"Part 1 Answer: {counter}");
-
-counter = 0;
-startingPoint = 50;
-
-foreach (var operation in operations)
-{
+  var answer = 0;
+  var startingPoint = 50;
+  foreach (var operation in operations)
+  {
     var t = operation[0];
     var value = int.Parse(operation.Trim(t));
     var timePassedZero = (value / 100);
 
     if (t.Equals('L'))
     {
-        // Minus value from startingPoint
-        startingPoint -= value;
+      // Minus value from startingPoint
+      startingPoint -= value;
     }
     else if (t.Equals('R'))
     {
-        // Add value to startingPoint
-        startingPoint += value;
+      // Add value to startingPoint
+      startingPoint += value;
     }
 
     if (startingPoint < 0)
     {
-        startingPoint = (startingPoint % 100);
-        counter++;
+      startingPoint = (startingPoint % 100);
+      answer++;
     }
     else if (startingPoint > 99)
     {
-        startingPoint = (startingPoint % 100);
-        counter++;
+      startingPoint = (startingPoint % 100);
+      answer++;
     }
     else if ((startingPoint % 100) == 0)
     {
-        counter++;
-        startingPoint = 0;
+      answer++;
+      startingPoint = 0;
     }
 
-    counter += timePassedZero;
+    answer += timePassedZero;
+  }
+
+  Console.WriteLine($"Part 2 Answer: {answer}");
 }
 
-Console.WriteLine($"Part 2 Answer: {counter}");
+static void Part1(string[] operations)
+{
+  var startingPoint = 50;
+  var answer = 0;
+
+  foreach (var operation in operations)
+  {
+    var t = operation[0];
+    var value = int.Parse(operation.Trim(t));
+
+    if (t.Equals('L'))
+    {
+      // Minus value from startingPoint
+      startingPoint -= value;
+    }
+    else if (t.Equals('R'))
+    {
+      // Add value to startingPoint
+      startingPoint += value;
+    }
+
+    if (startingPoint.Equals(0))
+    {
+      answer++;
+      continue;
+    }
+
+    if (startingPoint < 0)
+    {
+      startingPoint = (100 + startingPoint);
+    }
+
+    if ((startingPoint % 100) == 0)
+      answer++;
+  }
+
+  Console.WriteLine($"Part 1 Answer: {answer}");
+}
