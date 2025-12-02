ec037712fa63fa6e736b6de8e677dbe2e540781c
James Randle-Argyle <James.RandleArgyle@theaccessgroup.com>
2025-12-02 17:18:29 +0000

    Day 2 part 1 passed.

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -34,3 +34,45 @@ foreach (var range in ranges)
 
 Console.WriteLine($"Part 1 Answer: {answer}");
 
+answer = 0;
+
+foreach (var range in ranges)
+{
+    var lowwerBound = long.Parse(range.Split('-')[0].TrimStart('0'));
+    var upperBound = long.Parse(range.Split('-')[1].TrimStart('0'));
+
+    if (lowwerBound > upperBound)
+        Console.WriteLine("Invalid range");
+
+    var numbersWithinRange = new List<long>();
+
+    for (long i = lowwerBound; i <= upperBound; i++)
+    {
+        numbersWithinRange.Add(i);
+    }
+
+    foreach (var number in numbersWithinRange)
+    {
+        var str = number.ToString();
+
+        for (var c = 1; c <= (str.Length / 2); c++)
+        {
+            if (str.Length % c != 0)
+                continue;
+
+            var pattern = str.Substring(0, c);
+
+            var chunks = str.Select((c, i) => new { c, group = i / 3 })
+                         .GroupBy(x => x.group)
+                         .Select(g => new string(g.Select(x => x.c).ToArray()))
+                         .ToList();
+
+            if (chunks.All(x => x.Equals(pattern)))
+            {
+                answer += number;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Part 2 Answer: {answer}");
