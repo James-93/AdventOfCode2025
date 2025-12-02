ebed548bff46b1271b0bbcf813d6c09246c5880b
James Randle-Argyle <James.RandleArgyle@theaccessgroup.com>
2025-12-02 16:53:31 +0000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,78 @@
+﻿
+var counter = 0;
+var startingPoint = 50;
+var operations = File.ReadAllLines("input.txt");
+
+foreach (var operation in operations)
+{
+    var t = operation[0];
+    var value = int.Parse(operation.Trim(t));
+
+    if (t.Equals('L'))
+    {
+        // Minus value from startingPoint
+        startingPoint -= value;
+    }
+    else if (t.Equals('R'))
+    {
+        // Add value to startingPoint
+        startingPoint += value;
+    }
+
+    if (startingPoint.Equals(0))
+    {
+        counter++;
+        continue;
+    }
+
+    if (startingPoint < 0)
+    {
+        startingPoint = (100 + startingPoint);
+    }
+
+    if ((startingPoint % 100) == 0)
+        counter++;
+}
+
+Console.WriteLine($"Part 1 Answer: {counter}");
+
+counter = 0;
+startingPoint = 50;
+
+foreach (var operation in operations)
+{
+    var t = operation[0];
+    var value = int.Parse(operation.Trim(t));
+    var timePassedZero = (value / 100);
+
+    if (t.Equals('L'))
+    {
+        // Minus value from startingPoint
+        startingPoint -= value;
+    }
+    else if (t.Equals('R'))
+    {
+        // Add value to startingPoint
+        startingPoint += value;
+    }
+
+    if (startingPoint < 0)
+    {
+        startingPoint = (startingPoint % 100);
+        counter++;
+    }
+    else if (startingPoint > 99)
+    {
+        startingPoint = (startingPoint % 100);
+        counter++;
+    }
+    else if ((startingPoint % 100) == 0)
+    {
+        counter++;
+        startingPoint = 0;
+    }
+
+    counter += timePassedZero;
+}
+
+Console.WriteLine($"Part 2 Answer: {counter}");
---
+++ Day2/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllLines("input.txt");
+var ranges = input[0].Split(',');
+long answer = 0;
+
+foreach (var range in ranges)
+{
+    var lowwerBound = long.Parse(range.Split('-')[0].TrimStart('0'));
+    var upperBound = long.Parse(range.Split('-')[1].TrimStart('0'));
+
+    if (lowwerBound > upperBound)
+        Console.WriteLine("Invalid range");
+
+    var numbersWithinRange = new List<long>();
+
+    for (long i = lowwerBound; i <= upperBound; i++)
+    {
+        numbersWithinRange.Add(i);
+    }
+
+    foreach (var number in numbersWithinRange)
+    {
+        var str = number.ToString();
+
+        if (str.Length % 2 != 0)
+            continue;
+
+        var firstHalf = str.Substring(0, (str.Length / 2));
+        var secondHalf = str.Substring((str.Length / 2), (str.Length / 2));
+
+        if (firstHalf.Equals(secondHalf))
+            answer += number;
+    }
+}
+
+Console.WriteLine($"Part 1 Answer: {answer}");
+
