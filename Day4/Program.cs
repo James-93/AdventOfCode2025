5c110e2e03b896d2ba750797142d9a4426a4f2e5
James Randle-Argyle <James.RandleArgyle@theaccessgroup.com>
2025-12-04 10:03:41 +0000

    Day 4 final solution.

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,14 +1,114 @@
-﻿var input = File.ReadAllLines("input.txt");
+﻿
+var input = File.ReadAllLines("input.txt");
+var grid = new char[input[0].Length, input.Length];
 
-Part1(input);
-Part2(input);
-
-static void Part1(string[] input)
+for (var i = 0; i < input.Length; i++)
 {
-
+    for (var i2 = 0; i2 < input[i].Length; i2++)
+    {
+        grid[i,i2] = input[i][i2];
+    }
 }
 
-static void Part2(string[] input)
-{
+Part1(grid);
+Part2(grid);
 
+void Part1(char[,] grid)
+{
+    long answer = 0;
+
+    for (var i = 0; i < 138; i++)
+    {
+        for (var i2 = 0; i2 < 138; i2++)
+        {
+            if (grid[i, i2].Equals('@'))
+            {
+                var surroundingValues = new List<char>();
+
+                if (i > 0) // Square above
+                    surroundingValues.Add(grid[(i - 1), i2]);
+
+                if (i < 137) // Square below
+                    surroundingValues.Add(grid[(i + 1), i2]);
+
+                if (i2 > 0) // Square left
+                    surroundingValues.Add(grid[i, (i2 - 1)]);
+
+                if (i2 < 137) // Square right
+                    surroundingValues.Add(grid[i, (i2 + 1)]);
+
+                if (i > 0 && i2 > 0) // Square above and left
+                    surroundingValues.Add(grid[(i - 1), (i2 - 1)]);
+
+                if (i > 0 && i2 < 137) // Square above and right
+                    surroundingValues.Add(grid[(i - 1), (i2 + 1)]);
+
+                if (i < 137 && i2 > 0) // Square below and left
+                    surroundingValues.Add(grid[(i + 1), (i2 - 1)]);
+
+                if (i < 137 && i2 < 137) // Square below and right
+                    surroundingValues.Add(grid[(i + 1), (i2 + 1)]);
+
+                if (surroundingValues.Where(x => x.Equals('@')).Count() < 4)
+                    answer++;
+            }
+        }
+    }
+
+    Console.WriteLine($"Part 1 Answer: {answer}");
+}
+
+void Part2(char[,] grid)
+{
+    long answer = 0;
+    long addedThisLoop = 1;
+
+    while (addedThisLoop > 0)
+    {
+        addedThisLoop = 0;
+
+        for (var i = 0; i < 138; i++)
+        {
+            for (var i2 = 0; i2 < 138; i2++)
+            {
+                if (grid[i, i2].Equals('@'))
+                {
+                    var surroundingValues = new List<char>();
+
+                    if (i > 0) // Square above
+                        surroundingValues.Add(grid[(i - 1), i2]);
+
+                    if (i < 137) // Square below
+                        surroundingValues.Add(grid[(i + 1), i2]);
+
+                    if (i2 > 0) // Square left
+                        surroundingValues.Add(grid[i, (i2 - 1)]);
+
+                    if (i2 < 137) // Square right
+                        surroundingValues.Add(grid[i, (i2 + 1)]);
+
+                    if (i > 0 && i2 > 0) // Square above and left
+                        surroundingValues.Add(grid[(i - 1), (i2 - 1)]);
+
+                    if (i > 0 && i2 < 137) // Square above and right
+                        surroundingValues.Add(grid[(i - 1), (i2 + 1)]);
+
+                    if (i < 137 && i2 > 0) // Square below and left
+                        surroundingValues.Add(grid[(i + 1), (i2 - 1)]);
+
+                    if (i < 137 && i2 < 137) // Square below and right
+                        surroundingValues.Add(grid[(i + 1), (i2 + 1)]);
+
+                    if (surroundingValues.Where(x => x.Equals('@')).Count() < 4)
+                    {
+                        grid[i, i2] = '.';
+                        addedThisLoop++;
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+
+    Console.WriteLine($"Part 2 Answer: {answer}");
 }
